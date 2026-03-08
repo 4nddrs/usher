1607df6a3bf3e4059800b1baca1a3dc2f47fdfc6
Andrés Menchaca <112258012+4nddrs@users.noreply.github.com>
2026-03-08 18:35:06 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#app = Flask(__name__)
-#app.secret_key = os.environ.get("SECRET_KEY", "usher-logistics-dev-secret")
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "usher-logistics-dev-secret")
 
 # ── Firebase init ─────────────────────────────────────────────────────────────
 def _init_firebase():
